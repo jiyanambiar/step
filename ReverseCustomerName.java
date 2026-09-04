a0331806a2c926d1b060b75d06648e231ea80112
jiyanambiar <jiya.nambiar2024@gmail.com>
2026-09-04 23:17:10 +0530

    Create ReverseCustomerName.java

---
+++ ReverseCustomerName.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ReverseCustomerName {
+    public static String reverseCustomerName(String customerName) {
+        StringBuilder reversedName = new StringBuilder();
+        for (int i = customerName.length() - 1; i >= 0; i--) {
+            reversedName.append(customerName.charAt(i));
+        }
+        return reversedName.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter customer name: ");
+        String customerName = scanner.nextLine();
+        System.out.println("Original Name: " + customerName);
+        System.out.println("Reversed Name: " + reverseCustomerName(customerName));
+        scanner.close();
+    }
+}
